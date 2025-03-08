1a2d54a82b801bc69a61dd26783ca00b4e7c9543
tasmia_ <161472415+nehelin@users.noreply.github.com>
2025-03-09 00:11:00 +0600

    Delete PixieDixie directory

--- PixieDixie/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.pixiedixie {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.pixiedixie to javafx.fxml;
-    exports com.example.pixiedixie;
-}
